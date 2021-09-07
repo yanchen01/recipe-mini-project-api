0b6200acf0a373fb2a382705544b915471b78f8f
yanchen01 <ychen01@bu.edu>
2021-09-07 16:39:03 -0400

    added a search api route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
 app.use(cors());
 
 const PORT = 3000;
+
+app.post('/api/search', async (req, res) => {
+	const { query } = req.body;
+
+	const API_URL = 'https://api.nal.usda.gov/fdc/v1/foods';
+	try {
+		const response = await axios.get(`${API_URL}/search?api_key=DEMO_KEY&query=${JSON.stringify(query)}`);
+		res.send(response.data);
+	} catch (e) {
+		console.log(e);
+		res.status(400);
+	}
+});
 
 app.get('*', (req, res) => {
 	res.send('Hello World!');
